812e74a18d951c44ed8488484ab2afec626101f9
thermore <yvotherlar@gmail.com>
2014-10-03 14:13:34 +0200

    primerCommit

---
+++ ajaxPrueba/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ajaxPrueba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ajaxPrueba")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("189174be-df28-4b31-8f8c-56119b6bf9c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ajaxPrueba/script/js1.js
@@ -0,0 +1,50 @@
+﻿var url = "https://alumnos-mcsd2014.azure-mobile.net/Tables/alumnos";
+
+
+function pintarTabla(datos) {
+    var tabla = document.getElementById("datos");
+
+    while (tabla.childNodes.length > 0) {
+        tabla.removeChild(tabla.childNodes[0]); //borro la tabla por si tiene cosas
+    }
+    for (var i = 0; i < datos.length; i++) {
+        var fila = document.createElement("tr"); //creo una fila
+
+        var c1 = document.createElement("td"); //creo 3 td
+        var c2 = document.createElement("td");
+        var c3 = document.createElement("td");
+
+        var t1 = document.createTextNode(datos[i].nombre);
+        var t2 = document.createTextNode(datos[i].edad);
+        var t3 = document.createTextNode(datos[i].nota);
+
+        c1.appendChild(t1);
+        c2.appendChild(t2);
+        c3.appendChild(t3);
+
+        fila.appendChild(c1);
+        fila.appendChild(c2);
+        fila.appendChild(c3);
+
+        tabla.appendChild(fila); //a la tabla le agrego la fila
+    }
+}
+
+function cargar() {
+    var ajax = new XMLHttpRequest(); //creo un objeto ajax, siempre con el XMLHttpRequest();
+
+    ajax.open("get", url); //3 parámetros, (url, peticiones asíncronas(x defecto es true), password del usuario)
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {  //  4-->si hemos terminado la petición, que está listo
+            var datos = ajax.responseText;  //en datos tengo el texto devuelto
+            var res = eval(datos);  //lo convertimos en json
+            pintarTabla(res);
+        }
+    };
+    ajax.send(null); //le pongo siempre un objeto, por get siempre va por url
+}
+
+(function(){
+    cargar();
+})();
